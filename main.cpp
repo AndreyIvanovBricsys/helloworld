9f1787d0571e1da4977e8e4a0358a6b141a91f2b
Andrey Ivanov <andrey.ivanov@hexagon.com>
2021-08-22 18:29:51 +0700

    Used build number at compile time

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 
+#ifdef BUILD_NUMBER
+constexpr int buildNumber = BUILD_NUMBER;
+#else
+constexpr int buildNumber = std::numeric_limits<int>::max();
+#endif
+
 int main()
 {
-	std::cout << "Hello world" << std::endl;
+	std::cout << "build " << buildNumber << std::endl;
+	std::cout << "Hello, World!" << std::endl;
 	int dummyInput{ 0 };
 	std::cin >> dummyInput;
 
